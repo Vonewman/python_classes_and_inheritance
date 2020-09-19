e76d6f6641db8964f41b22bdd854627e0b6e3f93
vonewman <vonewman7@gmail.com>
2020-09-19 18:01:12 -0400

    add user-defined-classes

---
+++ week1/user-defined-classes/City.py
@@ -0,0 +1,24 @@
+cityNames = ['Detroit', 'Ann Arbor', 'Pittsburgh', 'Mars', 'New York']
+populations = [680250, 117070, 304391, 1683, 8406000]
+states = ['MI', 'MI', 'PA', 'PA', 'NY']
+
+city_tuples = zip(cityNames, populations, states)
+
+class City:
+    def __init__(self, n, p, s):
+        self.name = n
+        self.population = p
+        self.state = s
+
+    def __str__(self):
+        return '{}, {} (pop: {})'.format(self.name, self.state, self.population)
+
+
+#cities = []
+#for city_tup in city_tuples:
+#    name, pop, state = city_tup
+#    city = City(name, pop, state) # instance of the city classes
+#    cities.append(city)
+
+cities = [City(*t) for t in city_tuples]
+print(cities)
---
+++ week1/user-defined-classes/Point.py
@@ -0,0 +1,13 @@
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+point1 = Point(10, 100)
+print(point1.getX())
